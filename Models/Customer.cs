17bf69cccaf65d352fee16d6a1bc3d76e8ec5229
husninazuha <77769049+husninazuha@users.noreply.github.com>
2021-01-21 15:32:20 +0800

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace twoModelsInOneView.Models
+{
+    public class Customer
+    {
+        public int CustId { get; set; }
+        public string CustName { get; set; }
+        public string EmailAdd { get; set; }
+        public DateTime DteCreated { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace twoModelsInOneView.Models
+{
+    public class Product
+    {
+        public int ProdId { get; set; }
+        public string ProdTyp { get; set; }
+        public string price { get; set; }
+    }
+}
